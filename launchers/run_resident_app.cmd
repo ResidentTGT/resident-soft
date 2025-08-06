db75d8446b951bb67d400fcaad7b6d508b404bb9
resident <tromanov95@mail.ru>
2025-08-06 19:00:53 +0300

    fix launchers

--- launchers/run_resident_app.cmd
+++ launchers/run_resident_app.cmd
@@ -1,5 +1,4 @@
 @echo off
 
 set "NODE_OPTIONS=--disable-warning=DEP0040 --disable-warning=DEP0169"
-
 "%~dp0resident_app.exe" %*
--- launchers/run_resident_app.sh
+++ launchers/run_resident_app.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 export NODE_OPTIONS="--disable-warning=DEP0040 --disable-warning=DEP0169"
-
-# Запуск бинаря из той же директории, передавая все аргументы
+chmod +x "$(dirname "$0")/resident_app"
 exec "$(dirname "$0")/resident_app" "$@"
