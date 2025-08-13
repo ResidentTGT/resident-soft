49148933f29143a956cf608bea0c072378f9c42f
resident <tromanov95@mail.ru>
2025-08-13 23:14:23 +0300

    configs refactoring

---
+++ configs/eslint.config.mjs
@@ -0,0 +1,53 @@
+// @ts-check
+import path from 'node:path';
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+
+const ROOT = process.cwd();
+const TS_PROJECT = path.join(ROOT, 'tsconfig.json');
+
+const parserOptionsBlock = {
+	files: ['**/*.{ts,tsx}'],
+	languageOptions: {
+		globals: {
+			...globals.node,
+		},
+		parserOptions: {
+			project: [TS_PROJECT],
+			tsconfigRootDir: ROOT,
+			sourceType: 'module',
+			ecmaVersion: 'latest',
+		},
+	},
+};
+
+export default [
+	{
+		ignores: ['node_modules', 'dist/**', 'build/**', '**/*.js', 'configs/eslint.config.mjs'],
+	},
+	parserOptionsBlock,
+	...tseslint.config(eslint.configs.recommended, tseslint.configs.strict, tseslint.configs.stylistic, {
+		plugins: { 'unused-imports': unusedImports },
+
+		rules: {
+			'@typescript-eslint/no-extraneous-class': 'off',
+			'@typescript-eslint/prefer-for-of': 'warn',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'unused-imports/no-unused-imports': 'error',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+				},
+			],
+		},
+	}),
+];
