ddf66898d648eb2957960ab98cdcd4d92b0a0b7d
resident <tromanov95@mail.ru>
2025-11-28 19:54:34 +0300

    added twitter login by token

--- src/actions/groups/premium/twitter.actions.ts
+++ src/actions/groups/premium/twitter.actions.ts
@@ -10,7 +10,7 @@ export const twitterActions: ActionsGroup = {
 		{ action: ActionName.Follow, isolated: true, allowed: true, name: 'Подписаться' },
 		{ action: ActionName.LikeAndRetweet, isolated: true, allowed: true, name: 'Лайк и ретвит' },
 		{ action: ActionName.Login, isolated: true, allowed: true, name: 'Логин через логин/пароль' },
-		{ action: ActionName.LoginByToken, isolated: true, allowed: false, name: 'Логин по токену' },
+		{ action: ActionName.LoginByToken, isolated: true, allowed: true, name: 'Логин по токену' },
 		{ action: ActionName.Post, isolated: true, allowed: true, name: 'Написать пост' },
 		{ action: ActionName.Quote, isolated: true, allowed: true, name: 'Quote' },
 		{ action: ActionName.OutlookLogin, isolated: true, allowed: false, name: '' },
